8904c730024e6f58bb6a83b6f5381169648654cb
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-12-17 13:46:56 -0800

    V1

---
+++ AlertOnError.cs
@@ -0,0 +1,67 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace HD
+{
+  [InitializeOnLoad]
+  public class AlertOnError
+  {
+    double timeOfLastError = double.MinValue;
+
+    AudioClip clip;
+
+    /// <summary>
+    /// Called before the game starts c/o InitializeOnLoad.
+    /// </summary>
+    static AlertOnError()
+    {
+      AlertOnError alerter = new AlertOnError();
+    }
+
+    AlertOnError()
+    {
+      Application.logMessageReceived += OnLogMessage;
+      EditorApplication.playmodeStateChanged += OnPlaymodeChange;
+      InitClip();
+    }
+
+    void OnPlaymodeChange()
+    {
+      timeOfLastError = double.MinValue;
+    }
+
+    void InitClip()
+    {
+      if (clip != null)
+      {
+        return;
+      }
+
+      clip = Resources.Load<AudioClip>("Unity.ErrorAlert.ErrorAlertSound");
+      Debug.Assert(clip != null, 
+        "A resource audio clip named Unity.ErrorAlert.ErrorAlertSound is required");
+    }
+
+    void OnLogMessage(
+      string condition,
+      string stackTrace,
+      LogType type)
+    {
+      if (type == LogType.Log
+        || type == LogType.Warning)
+      { // Ignore 
+        return;
+      }
+
+      InitClip();
+
+      if((EditorApplication.timeSinceStartup - timeOfLastError) < clip.length)
+      { // Too soon
+        return;
+      }
+
+      AudioSource.PlayClipAtPoint(clip, Vector3.zero);
+      timeOfLastError = EditorApplication.timeSinceStartup;
+    }
+  }
+}
---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Unity.ErrorAlerts
+
+Never miss an error again!
+
+This simple, lightweight, and 100% free Unity asset will save you and your team hours.
+
+**When an error is encountered, a wav plays.**
+...I know.
+
+Tested with Unity 2017.  Works with both the old compiler and Unity's new C# 6 / .NET 4.6 support.
+
+
+## Install Instructions
+
+ - Download [Unity.ErrorAlerts Zip](TODO)
+ - Unzip into your Project's Asset directory.
+
+That's it!
+
+
+Optionally change the sound effect by replacing the file:
+
+  - Assets\HardlyDifficult\Unity.ErrorAlerts\Editor\Resources\HardlyDifficult.Unity.ErrorAlertSound.wav
+
+Note:
+
+ - There is absolutely NO impact on your final build.  The wav file and script are both Editor only.
+
+<br><br>
+
+Free for all, brought to you by twitch.tv/HardlyDifficult
